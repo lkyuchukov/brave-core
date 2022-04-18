d13e63fab35fcaf80294f7c260098db7c80e47fd
Aleksey Khoroshilov <akhoroshilov@brave.com>
2022-04-18 15:53:10 +0700

    Remove renderer rule updater in favor of our upstreamed impl.

--- chromium_src/components/content_settings/core/browser/content_settings_utils.cc
+++ chromium_src/components/content_settings/core/browser/content_settings_utils.cc
@@ -5,14 +5,23 @@
 
 #include "base/feature_list.h"
 #include "brave/components/brave_shields/common/brave_shield_constants.h"
+#include "components/content_settings/core/common/content_settings.h"
 
 #define BRAVE_GET_RENDER_CONTENT_SETTING_RULES                              \
+  DCHECK(RendererContentSettingRules::IsRendererContentSetting(             \
+      ContentSettingsType::AUTOPLAY));                                      \
   map->GetSettingsForOneType(ContentSettingsType::AUTOPLAY,                 \
                              &(rules->autoplay_rules));                     \
+  DCHECK(RendererContentSettingRules::IsRendererContentSetting(             \
+      ContentSettingsType::BRAVE_FINGERPRINTING_V2));                       \
   map->GetSettingsForOneType(ContentSettingsType::BRAVE_FINGERPRINTING_V2,  \
                              &(rules->fingerprinting_rules));               \
+  DCHECK(RendererContentSettingRules::IsRendererContentSetting(             \
+      ContentSettingsType::BRAVE_SHIELDS));                                 \
   map->GetSettingsForOneType(ContentSettingsType::BRAVE_SHIELDS,            \
                              &(rules->brave_shields_rules));                \
+  DCHECK(RendererContentSettingRules::IsRendererContentSetting(             \
+      ContentSettingsType::BRAVE_COSMETIC_FILTERING));                      \
   map->GetSettingsForOneType(ContentSettingsType::BRAVE_COSMETIC_FILTERING, \
                              &(rules->cosmetic_filtering_rules));
 
